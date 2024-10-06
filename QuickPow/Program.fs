95ff524a18df09feb1b0318156d15f0121a75c1c
Mihkail Barsukov <hardbasser322@gmail.com>
2024-10-06 13:34:42 +0700

    Реализация быстрого возведения в степень

---
+++ QuickPow/Program.fs
@@ -0,0 +1,7 @@
+﻿open System.Diagnostics
+let rec quickPow x y m acc =
+    match (y % 2) with
+    | 0 when y = 0 -> acc
+    | 0 -> quickPow (x * x % m) (y / 2) m acc 
+    | 1 -> quickPow (x * x % m) ((y - 1) / 2) m (acc * x % m)
+    | _ -> raise (UnreachableException("something went wrong!"))
